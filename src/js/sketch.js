11d5ec1a6b2f6730254fe1fd7e853c5ef2b8ac17
Bereket Semagn <semagnbereket@gmail.com>
2020-09-20 20:50:23 -0400

    updates

--- src/js/sketch.js
+++ src/js/sketch.js
@@ -1,5 +1,32 @@
-var d = new Date();
-document.getElementById("demo").innerHTML = d;
+function showTime(){
+  var date = new Date();
+  var h = date.getHours(); // 0 - 23
+  var m = date.getMinutes(); // 0 - 59
+  var s = date.getSeconds(); // 0 - 59
+  var session = "AM";
+  
+  if(h == 0){
+      h = 12;
+  }
+  
+  if(h > 12){
+      h = h - 12;
+      session = "PM";
+  }
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  s = (s < 10) ? "0" + s : s;
+  
+  var time = h + ":" + m + ":" + s + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+  
+  setTimeout(showTime, 1000);
+  
+}
+
+showTime();
 
 // A random word
 var randomWordURL = "http://api.wordnik.com/v4/words.json/randomWord?" +
@@ -19,3 +46,4 @@ function wordnik(where, url) {
     div.parent(where);
   }
 }
+
